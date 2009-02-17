b515daa4fd7734e329970df877d415debbd4f302
punkytse <punkytse@fc1de9d0-7410-4adc-bb29-da8c0547dad5>
2009-02-17 06:22:35 +0000

    0.6.0 release

---
+++ 0.6.0/config/chroot_local-hooks/001-rmpkg.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+RMPKGS="at \
+bsdmainutils \
+ed \
+fdutils \
+groff-base \
+info \
+libtext-charwidth-perl \
+libtext-wrapi18n-perl \
+libtext-iconv-perl \
+manpages \
+nano \
+aptitude \
+tasksel \
+tasksel-data \
+liblocale-gettext-perl \
+libcwidget3 \
+libsigc++-2.0-0c2a \
+console-data \
+console-common \
+kbd \
+libconsole \
+apt-utils \
+libgnutls13 \
+libgdbm3 \
+libdb4.4 \
+dmidecode \
+gcc-4.2-base \
+libnewt0.52 \
+gettext-base \
+dialog \
+libncursesw5 \
+libgpmg1 \
+libept0 \
+libxapian15 \
+vim-tiny \
+vim-common \
+locales \
+netcat-traditional \
+liblzo2-2 \
+dhcp3-client \
+dhcp3-common \
+"
+
+for PKG in $RMPKGS
+do
+	apt-get -f -y -q=2 --purge remove $PKG
+done
+
+exit
+
+# below are harmful
+libbz2-1.0 \
+libtasn1-3 \
+libgcrypt11 \
+libopencdk10 \
+libgpg-error0 \
+libusb-0.1-4 \
+hostname \
+libldap-2.4-2 \
