7f392b49885e1c86ffe8938b1a409c7feb23c2e4
punkytse <punkytse@fc1de9d0-7410-4adc-bb29-da8c0547dad5>
2010-11-02 03:53:47 +0000

    change prism54 firmware URL

--- voyage-live/config/chroot_local-hooks/020-install_prismfw.sh
+++ voyage-live/config/chroot_local-hooks/020-install_prismfw.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-PRISM54_FIRMWARE_URL="http://jbnote.free.fr/prism54usb/data/firmwares/p54pci_1.0.4.3.arm"
+#PRISM54_FIRMWARE_URL="http://jbnote.free.fr/prism54usb/data/firmwares/p54pci_1.0.4.3.arm"
 #PRISM54_FIRMWARE_URL="http://prism54.org/~mcgrof/firmware/1.0.4.3.arm"
 #PRISM54_FIRMWARE_URL="http://ruslug.rutgers.edu/~mcgrof/802.11g/firmware/1.0.4.3.arm"
+PRISM54_FIRMWARE_URL="http://lekernel.net/prism54/firmware/1.0.4.3.arm"
 
 echo -n "Downloading Firmware from Prism54.org ... "
 
