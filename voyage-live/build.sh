371b365ac58a93bca1bd15bf5ee49142eb5bc7a0
punkytse <punkytse@fc1de9d0-7410-4adc-bb29-da8c0547dad5>
2012-12-11 09:34:13 +0000

    change to 3.6.9 kernel

--- voyage-live/build.sh
+++ voyage-live/build.sh
@@ -7,8 +7,8 @@ MOUNT_PROC_SH=/usr/local/sbin/mount-proc.sh
 export MKSQUASHFS_OPTIONS="-b 1048576"
 
 # define linux packages here for different editions
-VOYAGE_LINUX_PACKAGES="linux-image-3.6.8"
-ONE_LINUX_PACKAGES="linux-image-3.6.8 dahdi-modules-3.6.8"
+VOYAGE_LINUX_PACKAGES="linux-image-3.6.9"
+ONE_LINUX_PACKAGES="linux-image-3.6.9 dahdi-modules-3.6.9"
 MPD_LINUX_PACKAGES=$VOYAGE_LINUX_PACKAGES
 
 if [ $(uname -m) == "x86_64" ] ; then
