62c009e73be92082de5921bd83e085d93f4eee8c
punkytse <punkytse@fc1de9d0-7410-4adc-bb29-da8c0547dad5>
2013-05-10 02:23:15 +0000

    voyage-util: fix autoinstall filesystem.squashfs not mounted

--- voyage-util/overlay_fs/usr/local/sbin/autoinstall.sh
+++ voyage-util/overlay_fs/usr/local/sbin/autoinstall.sh
@@ -50,7 +50,7 @@ if [ ! -d $TARGET_MOUNT ] ; then mkdir $TARGET_MOUNT ; fi
 umount $DISTDIR > /dev/null
 umount $TARGET_MOUNT > /dev/null
 
-SQFS=$(find / -name "filesystem.squashfs" | head -n1)
+SQFS=$(find / -type f -name "filesystem.squashfs" | head -n1)
 if [ ! -z $SQFS ] ; then
 	mount -o loop $SQFS $DISTDIR
 else
