f5f98fa4f99e8e3a51e91c5a1de1af1811a0a708
punkytse <punkytse@fc1de9d0-7410-4adc-bb29-da8c0547dad5>
2013-05-15 11:35:45 +0000

    change to 3.8.12 kernel

--- voyage-live/build.sh
+++ voyage-live/build.sh
@@ -7,8 +7,8 @@ MOUNT_PROC_SH=/usr/local/sbin/mount-proc.sh
 export MKSQUASHFS_OPTIONS="-b 1048576"
 
 # define linux packages here for different editions
-VOYAGE_LINUX_PACKAGES="linux-image-3.8.11"
-ONE_LINUX_PACKAGES="linux-image-3.8.11 dahdi-modules-3.8.11"
+VOYAGE_LINUX_PACKAGES="linux-image-3.8.12"
+ONE_LINUX_PACKAGES="linux-image-3.8.12 dahdi-modules-3.8.12"
 MPD_LINUX_PACKAGES=$VOYAGE_LINUX_PACKAGES
 
 if [ $(uname -m) == "x86_64" ] ; then
